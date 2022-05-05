ec90ecffe61f14aa4b16fe18ee9b45088edd31e2
ytraiba <ytraiba@umass.edu>
2022-05-05 17:52:55 -0400

    1
    1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 const Skills = () => {
 
     return (
-    <div name='listenHome' className='w-full h-screen sm:h-[70vh] bg-hero-listen text-white bg-cover'>
-        <div className='w-full flex flex-col mx-auto h-full items-center border border-b-[50px] border-[#b5b5b5] justify-center'>
-        
-        <p className='text-6xl font-bold inline pt-20'>LISTEN</p>
+    <div name='listenHome' className='w-full h-screen sm:h-[70vh] bg-hero-listen text-white bg-cover border border-b-[50px] border-[#b5b5b5]'>
+        <div className='flex flex-col mx-auto h-full items-center justify-center'>
+        <p className='text-6xl font-bold sm:font-extrabold sm:text-7xl inline pt-20'>LISTEN</p>
         <br/>
         <p className='text-3xl text-pb-20'>- podcast episodes -</p>
-        
-
+        <button className='text-white group border-2 border-white] px-6 py-3 my-2 flex items-center hover:text-yellow-300 hover:border-white'>
+            Subscribe
+        </button>
         </div>
+        
     </div>
   );
 };
--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -10,9 +10,9 @@ const Episodes = () => {
     const GridEntry = props => {
         return <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-auto py-4'>
             <div className='flex flex-col justify-center items-center h-full text-lg'>
-            <p className='mb-4'>{props.name}</p>
+            <p className='text-xl mb-4'>{props.name}</p>
             <img className='w-40 sm:w-52 mx-auto' src={props.img} alt={props.alt} />
-            <p className='mt-4'>{props.asset}</p>
+            <p className='text-sm mt-4'>{props.asset}</p>
             </div>
         </div>
     };
@@ -22,17 +22,17 @@ const Episodes = () => {
 
       <div className='max-w-[1000px] mx-auto p-4 pt-[100px] flex flex-col justify-center w-full'>
           <div>
-              <p className='text-4xl font-bold inline border-b-4 border-[#b625d9] '>Episodes</p>
-              <p className='py-4'> Season 1</p>
+              <p className='text-4xl font-bold inline border-b-4 border-[#b625d9] '>Season One</p>
+              <p className='py-4'>Episodes</p>
           </div>
 
           <div className='grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-            <GridEntry name="SZN1 EP1"  asset="March 22nd 2022" img={EP1} alt="EP1 icon" />
-            <GridEntry name="SZN1 EP2"  asset="March 22nd 2022" img={EP2} alt="EP1 icon" />
-            <GridEntry name="SZN1 EP3"  asset="March 22nd 2022" img={EP3}alt="EP1 icon" />
-            <GridEntry name="SZN1 EP4"  asset="March 22nd 2022" img={EP4}alt="EP1 icon" />
-            <GridEntry name="SZN1 EP5"  asset="March 22nd 2022" img={EP5}alt="EP1 icon" />
-            <GridEntry name="SZN1 EP6"  asset="March 22nd 2022" img={EP6}alt="EP1 icon" />
+            <GridEntry name="Episode 1"  asset="March 22nd 2022" img={EP2} alt="EP1 icon" />
+            <GridEntry name="Episode 2"  asset="March 23rd 2022" img={EP1} alt="EP1 icon" />
+            <GridEntry name="Episode 3"  asset="March 24th 2022" img={EP3}alt="EP1 icon" />
+            <GridEntry name="Episode 4"  asset="March 25th 2022" img={EP4}alt="EP1 icon" />
+            <GridEntry name="Episode 5"  asset="March 26th 2022" img={EP5}alt="EP1 icon" />
+            <GridEntry name="Episode 6"  asset="March 27th 2022" img={EP6}alt="EP1 icon" />
           </div> 
       </div>
     </div>
