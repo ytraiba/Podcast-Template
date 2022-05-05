6a0664366d4a960f4368a77e8d7e17e6e8f4c1a6
ytraiba <ytraiba@umass.edu>
2022-05-05 16:42:00 -0400

    1
    1

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -5,7 +5,7 @@ const Episodes = () => {
     const GridEntry = props => {
         return <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-72'>
             <div className='flex flex-col justify-center items-center h-full text-lg'>
-            {/* <img className='w-24 mx-auto' src={props.asset} alt={props.alt} /> */}
+            <img className=' w-auto mx-auto' src={props.asset} alt={props.alt} />
             <p className='my-4'>{props.name}</p>
             <p className='my-4'>{props.asset}</p>
             </div>
@@ -13,7 +13,7 @@ const Episodes = () => {
     };
 
     return (
-    <div name='episodes' className='w-full h-[180vh] sm:h-[120vh] bg-gray-200 text-black'>
+    <div name='episodes' className='w-full h-[150vh] sm:h-[120vh] bg-gray-200 text-black'>
 
       <div className='max-w-[1000px] mx-auto p-4 pt-[100px] flex flex-col justify-center w-full'>
           <div>
@@ -22,7 +22,7 @@ const Episodes = () => {
           </div>
 
           <div className='grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-            <GridEntry name="SZN1 EP1" asset="March 22nd 2022" alt="HTML icon" />
+            <GridEntry name="SZN1 EP1" asset="March 22nd 2022" alt="EP1 icon" />
             <GridEntry name="EP2"  alt="CSS icon" />
             <GridEntry name="EP3"  alt="JavaScript icon" />
             <GridEntry name="EP4"  alt="React icon" />
