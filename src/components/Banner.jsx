a11087cd1c3244c049a51fdc840638e29916ba41
ytraiba <ytraiba@umass.edu>
2022-05-05 23:09:08 -0400

    1
    1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Skills = () => {
 
     return (
-    <div name='listenHome' className='w-full h-screen sm:h-[70vh] bg-hero-listen text-white bg-cover bg-fixed '>
-        <div className='flex mx-auto h-full items-center justify-center p-0 pt-8 sm:pl-28'>
+    <div name='listenHome' className='w-full h-screen sm:h-[55vh] bg-hero-listen text-white bg-cover bg-fixed '>
+        <div className='flex mx-auto h-full items-center justify-center p-0 pt-28 sm:pl-28'>
         <section className='place-content-center items-center mr-4'>
         <p className='text-8xl font-extrabold sm:font-extrabold sm:text-7xl inline '>LISTEN</p>
         <p className='text-4xl text-pb-20 pt-2 text-yellow-300'>podcast episodes</p>
