dde15352d507740239c6ba3f5dd2afba3a32d01e
ytraiba <ytraiba@umass.edu>
2022-05-22 11:28:41 -0400

    1
    1

--- src/components/SeasonOne/SznOneBanner.jsx
+++ src/components/SeasonOne/SznOneBanner.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div name='listenHome' className='w-full h-[80vh] sm:h-[35vh] bg-hero-listen text-white bg-cover bg-fixed '>
         <div className='flex mx-auto h-full items-end justify-center pb-72 sm:pb-0'>
         <section className='place-content-center items-center mr-4'>
-        <p className='text-8xl font-extrabold sm:font-extrabold sm:text-7xl inline'>Season One</p>
+        <p className='text-5xl font-extrabold sm:font-extrabold sm:text-7xl inline'>Season One</p>
         </section> 
         </div>
     </div>
